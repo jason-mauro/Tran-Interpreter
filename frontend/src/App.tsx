456d9c8b10ca2320dd54e2e0d357bbace470b437
Jason Mauro <jamauro619@gmail.com>
2024-12-23 00:22:40 -0500

    updated shadcn (might need to redo)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,17 @@
-import Canvas from './components/Canvas'
-
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from "../components/ui/resizable"
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "../components/ui/card"
-
-
+import * as React from "react"
+ 
+import { Calendar } from "../components/ui/calendar"
 
 function App() {
+  const [date, setDate] = React.useState<Date | undefined>(new Date())
+ 
   return (
-    <><ResizablePanelGroup direction="horizontal">
-    <ResizablePanel><Canvas></Canvas></ResizablePanel>
-    <ResizableHandle />
-    <ResizablePanel><Card>
-  <CardHeader>
-    <CardTitle>Card Title</CardTitle>
-    <CardDescription>Card Description</CardDescription>
-  </CardHeader>
-  <CardContent>
-    <p>Card Content</p>
-  </CardContent>
-  <CardFooter>
-    <p>Card Footer</p>
-  </CardFooter>
-</Card>
-</ResizablePanel>
-    </ResizablePanelGroup>
-    </>
+    <Calendar
+      mode="single"
+      selected={date}
+      onSelect={setDate}
+      className="rounded-md border shadow"
+    />
   )
 }
 

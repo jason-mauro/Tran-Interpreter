1f679edaf9aabf86217ca7e7d56b38532829259d
Jason Mauro <jamauro619@gmail.com>
2024-12-31 03:36:03 -0500

    Updated UI and created the tabbed text editor for coding

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,87 +1,15 @@
-"use client"
-import { Button } from "@/components/ui/button"
-import { ThemeProvider } from "./components/theme-provider"
-import Canvas from "@/components/Canvas"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
+"use client";
 
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from "@/components/ui/resizable"
-
+import Editor from '@/components/Editor';
+import { ThemeProvider } from './components/theme-provider';
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-
-<ResizablePanelGroup
-      direction="horizontal"
-      className="min-h-[200px] rounded-lg border md:min-w-[450px]"
-    >
-      <ResizablePanel defaultSize={25}>
-        <div className="flex h-full items-center justify-center">
-          <Canvas></Canvas>
-        </div>
-      </ResizablePanel>
-      <ResizableHandle withHandle />
-      <ResizablePanel defaultSize={75}>
-        <div className="flex h-full items-center justify-center p-6">
-        <Card className="w-[350px]">
-        
-        <CardHeader>
-          <CardTitle>Create project</CardTitle>
-          <CardDescription>Deploy your new project in one-click.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">Name</Label>
-                <Input id="name" placeholder="Name of your project" />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="framework">Framework</Label>
-                <Select>
-                  <SelectTrigger id="framework">
-                    <SelectValue placeholder="Select" />
-                  </SelectTrigger>
-                  <SelectContent position="popper">
-                    <SelectItem value="next">Next.js</SelectItem>
-                    <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                    <SelectItem value="astro">Astro</SelectItem>
-                    <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
-            </div>
-          </form>
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          <Button variant="outline">Cancel</Button>
-          <Button>Deploy</Button>
-        </CardFooter>
-      </Card>
-        </div>
-      </ResizablePanel>
-    </ResizablePanelGroup>
-      
+      <div className="w-full h-5 bg-primary"></div>
+      <div className="flex flex-row justify-center items-center w-full h-[800px] px-[225px] py-10">
+          <Editor />
+      </div>
     </ThemeProvider>
   );
 }
